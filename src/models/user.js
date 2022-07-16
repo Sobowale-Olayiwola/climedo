c687c60efb6b7bb501e5acc13212ad3e53835913
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-07-16 10:07:07 +0100

    feat: added user model

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import constants from '../config/constants';
+
+const { USER_ROLES } = constants;
+const { model, Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+    },
+    role: {
+      type: String,
+      enum: [...USER_ROLES],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const User = model('User', userSchema);
+export default User;
